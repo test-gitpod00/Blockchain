3ee8156f2974f9b556938dff9fcc77ccff2ba0ae
Adriana <hrl.davidson@gmail.com>
2022-03-22 10:52:26 +0000

    3

--- block.js
+++ block.js
@@ -1,3 +1,9 @@
+const SHA256 = require("crypto-js/sha256")  // Import | Verweis
+
+
+
+
+
 class Block{
 
     constructor(timestamp,lastHash,hash,data){
@@ -11,22 +17,26 @@ class Block{
        return `Block -
        Timestamp:   ${this.timestamp}
        Last Hash:   ${this.lastHash.substring(0,10)}
+       BigHash:     ${this.hash}
        Hash:        ${this.hash.substring(0,10)}
        Data         ${this.data}`; 
     }
     static genesis(){
-        return new this("Genesis time", "-----","abcd1234",[]);
+        return new this("Genesis time", "-----","abcd1234",["data1,data2"]);
     }
    
 static mineBlock(lastBlock,blockData){
 
     const timestamp = Date.now(); // Zeit in ms seit dem 01.01.1970
     const lastHash = lastBlock.hash; // Hash des vorhergehenden Blocks
-    const hash = "todo Hash"; // hash; // Eigener Hash
+    const hash = Block.hash(timestamp,lastHash,blockData); // hash; // Eigener Hash
     const data = blockData; // Daten des Blocks
 
     return new this(timestamp,lastHash,hash,data);
-}
+    }
+    static hash(timestamp,lastHash,hash,data){
+        return SHA256(`${timestamp}${lastHash}${data}`).toString();
+    }
 }
 
 module.exports = Block;
--- dev-test.js
+++ dev-test.js
@@ -6,3 +6,4 @@ const block = new Block("time1","hash1","hash2","data");
 
 // console.log(block.toString());
 console.log(Block.genesis().toString()); 
+
