6dc7fcfe4eca2bf2cdd6ca5dc04a72cda4fdd716
Adriana <93542455+Adrianna7395@users.noreply.github.com>
2022-03-22 10:31:00 +0100

    Delete dev-test.js

--- dev-test.js
+++
@@ -1,9 +0,0 @@
-const Block = require("./block");
-
-//const block = new Block("time", "hash1", "hash2", "data");
-
-//console.log(block.toString());
-console.log(Block.genesis().toString());
-
-const testBlock = Block.mineBlock(Block.genesis(),["data1,data2"]);
-console.log(testBlock.toString());
